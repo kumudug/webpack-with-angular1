918c8c4e771bae7c635f475b9c92d7bbad865dcf
Kumudu Gunasekara <Kumudu Gunasekara>
2016-09-13 23:01:20 +1000

    housekeeping

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
 var angular = require('angular');
 var app = angular.module('app', []);
-require('./bands/band-info')(app);
-require('./bands/band-list')(app);
+
+require('./bands')(app); //webpack will require the index.js file inside the bands folder
